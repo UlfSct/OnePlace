248b9baf64440fcabe50fb7faaa6db2c9c13e692
Pater-Forg <gbogdanovg@gmail.com>
2022-02-15 00:58:56 +0300

    Mapa

--- dbinit.py
+++ dbinit.py
@@ -6,23 +6,5 @@ conn = sqlite3.connect('oneplace.db')
 with open('schema.sql') as f:
     conn.executescript(f.read())
 
-cur = conn.cursor()
-
-cur.execute("INSERT INTO users (username, password) VALUES (?, ?)",
-            ('Abobus', '12345')
-            )
-
-lastID = cur.lastrowid
-
-cur.execute("INSERT INTO userData (userID) VALUES (?)", (str(lastID)))
-
-cur.execute("INSERT INTO users (username, password) VALUES (?, ?)",
-            ('_Cool_', '3221337')
-            )
-
-lastID = cur.lastrowid
-
-cur.execute("INSERT INTO userData (userID) VALUES (?)", (str(lastID)))
-
 conn.commit()
 conn.close()
--- main.py
+++ main.py
@@ -5,7 +5,18 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.app import App
 from kivymd.app import MDApp
 from kivymd.uix.textfield import MDTextFieldRound
-import sqlite3
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty, NumericProperty, ObjectProperty
+import database as db
+from userClass import User
+from kivy_garden.mapview import MapView, MapMarker, MapLayer, MarkerMapLayer, MapMarkerPopup
+from kivy.uix.button import Button
+from kivymd.uix.label import MDLabel
+from kivy.uix.boxlayout import BoxLayout
+
+
+db_name = 'oneplace.db'
+user = User()
 
 
 class WindowManager(ScreenManager):
@@ -13,55 +24,89 @@ class WindowManager(ScreenManager):
 
 
 class LogWindow(Screen):
-    def verify(self, db, username, password):
-        isOK = False
-        conn = sqlite3.connect(db)
-        conn.row_factory = sqlite3.Row
-        cur = conn.cursor()
-        usr = cur.execute("SELECT * FROM users WHERE username=?", (username,)).fetchone()
-        if usr and usr['password'] == password:
-            isOK = True
-        conn.close()
-        return isOK
+    user = ObjectProperty(user)
+
+    def write_data_in_user(self, data):
+        self.user.userID = data['userID']
+        self.user.username = data['username']
+        self.user.money = data['money']
+        self.user.exp = data['exp']
+        for i, val in enumerate([data['plant1'], data['plant2'], data['plant3'], data['plant4']]):
+            self.user.plants[i] = val
 
     def login_user(self, usr, pwd):
-        if self.verify('oneplace.db', usr, pwd):
+        data = db.get_user_from_db(db_name, usr, pwd)
+        if data:
+            self.write_data_in_user(data)
             MDApp.get_running_app().root.current = 'main'
         else:
             MDApp.get_running_app().root.current = 'failedlog'
 
 
 class RegWindow(Screen):
-    def check(self, db, username):
-        isOK = True
-        conn = sqlite3.connect(db)
-        conn.row_factory = sqlite3.Row
-        cur = conn.cursor()
-        usr = cur.execute("SELECT * FROM users WHERE username=?", (username,)).fetchone()
-        if usr:
-            isOK = False
-        conn.close()
-        return isOK
-
     def register_user(self, usr, pwd):
-        if self.check('oneplace.db', usr):
-            conn = sqlite3.connect('oneplace.db')
-            cur = conn.cursor()
-            cur.execute("INSERT INTO users (username, password) VALUES (?, ?)", (usr, pwd))
-            cur.execute("INSERT INTO userData (userID) VALUES (?)", str(cur.lastrowid))
-            conn.commit()
-            cur.close()
-            conn.close()
-            MDApp.get_running_app().root.current = 'sucreg'
-        else:
+        if db.is_user_exists(db_name, usr):
             MDApp.get_running_app().root.current = 'failedreg'
+        else:
+            db.add_user_to_db(db_name, usr, pwd)
+            MDApp.get_running_app().root.current = 'sucreg'
+
+
+class MainWindow(Screen):
+    user = ObjectProperty(user)
+    tasks = []
+    map = ObjectProperty(None)
+    markers = None
+
+    def build_map(self):
+        data = db.get_markers(db_name)
+        self.markers = [
+            MapMarkerPopup(lat=row['lat'], lon=row['lon'], source='img/map/map-marker.ico') for row in data
+        ]
+        for mp in self.markers:
+            mp.add_widget(MDLabel(text='ffff'))
+            self.map.add_widget(mp)
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.tasks = db.get_daily_tasks(db_name, 3)
+
+
+class MapWindow(Screen):
+    map = ObjectProperty(None)
+    # layer1 = None
+    # layer2 = None
+    # mp1 = None
+    # mp2 = None  # MapMarker(lat=36.5, lon=-110.7, source='img/map/map_marker.ico')
+    #
+    # def add_map(self):
+    #     self.layer1 = MarkerMapLayer()
+    #     self.mp1 = MapMarker(lat=36, lon=-110, source='img/map/map_marker.ico')
+    #     self.map.add_marker(self.mp1, layer=self.layer1)
+    #     self.map.add_layer(self.layer1)
+    #
+    # def change_layer(self):
+    #     self.map.remove_widget(self.layer1)
+    #     self.layer2 = MarkerMapLayer()
+    #     self.mp2 = MapMarker(lat=36.5, lon=-110.7, source='img/map/map_marker.ico')
+    #     self.map.add_marker(self.mp2, layer=self.layer2)
+    #     self.map.add_layer(self.layer2)
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
 
 
 Config.set('graphics', 'resizable', False)
 Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '600')
 
+
 class OnePlaceApp(MDApp):
+    user = ObjectProperty(user)
+
+    def on_stop(self):
+        db.save_user_data(db_name, self.user)
+
     def build(self):
         Window.size = (500, 780)
         return Builder.load_file("onePlace.kv")
@@ -69,3 +114,5 @@ class OnePlaceApp(MDApp):
 
 if __name__ == '__main__':
     OnePlaceApp().run()
+
+
